c1aeb78f38ed154c607c32e70c12161e637f3c3b
Tianlionfire <1019426433@qq.com>
2018-08-04 17:27:30 +0800

    test

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -104,7 +104,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY','jk0RUo8OAYSANeUBN+aSaG+AV6ty//mb4IVxaEefW2A='),
+    'key' => env('APP_KEY','1l92qvcLnI5i/A0qk1fPXvyzTo2qnU3xRVgh7WbtsaQ='),
 
     'cipher' => 'AES-256-CBC',
 
